dee3e9aa4660e74a7e227eeba1c04855e2bc3253
yarnedb539 <128077625+yarnedb539@users.noreply.github.com>
2026-09-10 18:53:29 +0200

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        mid = (l+r) // 2
+        while l<=r:
+            if target == nums[mid]:
+                return mid
+            if target < nums[mid]:
+                r = mid - 1
+            if target > nums[mid]:
+                l = mid + 1
+            mid = (l+r) // 2
+        return -1
